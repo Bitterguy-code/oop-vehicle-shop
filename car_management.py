03682f2e2474b19b5008be23f17a12f63c521471
bitterguy-code <jasonb4cake@gmail.com>
2025-02-11 11:06:23 -0500

    added initial class skeleton

--- car_management.py
+++ car_management.py
@@ -1,2 +1,11 @@
 class CarManager:
-    pass
+    all_cars = {}
+    total_cars = 0
+
+    def __init__(self):
+        _id = 0
+        _make = ""
+        _model = ""
+        _year = 0
+        _mileage = 0
+        _services = []
